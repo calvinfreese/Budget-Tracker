71c62e9d79fe12906c79d6a2583aa5ef081ac27a
whackingMUFN <46834593+whackingMUFN@users.noreply.github.com>
2020-11-12 21:45:04 -0600

    deploying

--- server.js
+++ server.js
@@ -15,10 +15,16 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/workoutsdb", {
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/workoutsdb",
+{ 
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
-});
+
+}
+);
 
 // routes
 app.use(require("./routes/api.js"));
